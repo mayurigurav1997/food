63f12eea510ee200e4bead2749112c2a81ba2193
Mayuri Gurav <mayurigurav1997@gmail.com>
2023-06-01 19:56:36 +0530

    hooks in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 // import React from 'react'
 import Restuarant from './component/Basics/Restuarant'
+import UseState from './component/Hooks/useState'
+import UseEffect from './component/Hooks/useEffect'
+import UseReducer from './component/Hooks/useReducer'
 
 const App = () => {
   return (
     <>
-      <Restuarant />
+      <UseReducer />
+      {/* <UseEffect /> */}
+      {/* <UseState /> */}
+      {/* <Restuarant /> */}
     </>
   )
   // return React.createElement("div", {}, "Hello World")
